5356288402ddd59280042e14ff189c49d5060a19
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-02-03 15:15:12 -0500

    Paquetes

---
+++ Paquetes/src/mx/com/globalmentoring/Utileria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.globalmentoring;
+
+/**
+ *
+ * @author lepo9
+ */
+public class Utileria {
+    public static void imprimir(String s){
+    
+        System.out.println("s = " + s);
+    }
+}
